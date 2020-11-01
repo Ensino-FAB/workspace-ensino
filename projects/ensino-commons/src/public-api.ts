6e5c8ccb551c143c8a2af40940e3674f76abac1c
Fernando Tomasio <fernandotomasio@gmail.com>
2020-11-01 12:28:41 -0300

    refactor(ensino-commons): :hammer: atualiza a api publica

--- projects/ensino-commons/src/public-api.ts
+++ projects/ensino-commons/src/public-api.ts
@@ -1,3 +1,14 @@
 /*
  * Public API Surface of ensino-commons
  */
+
+// Components
+
+export * from './lib/components/sub-nav/sub-nav.component';
+export * from './lib/components/sub-nav/sub-nav.module';
+
+export * from './lib/components/not-found/not-found.component';
+export * from './lib/components/not-found/not-found.module';
+
+export * from './lib/components/not-mobile-responsive/not-mobile-responsive.component';
+export * from './lib/components/not-mobile-responsive/not-mobile-responsive.module';
