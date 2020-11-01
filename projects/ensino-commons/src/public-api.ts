1bb3d19c3a16e4cc10d7154c4ca4525ab58f1555
Fernando Tomasio <fernandotomasio@gmail.com>
2020-11-01 10:39:30 -0300

    refactor(ensino-commons): :hammer: limpa api publica

--- projects/ensino-commons/src/public-api.ts
+++ projects/ensino-commons/src/public-api.ts
@@ -1,7 +1,3 @@
 /*
  * Public API Surface of ensino-commons
  */
-
-export * from './lib/ensino-commons.service';
-export * from './lib/ensino-commons.component';
-export * from './lib/ensino-commons.module';
