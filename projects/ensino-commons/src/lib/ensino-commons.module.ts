2ff3592650c0a9ff99e25bc5a900b400ef0cfdac
Fernando Tomasio <fernandotomasio@gmail.com>
2020-11-01 10:36:56 -0300

    refactor(ensino-commons): :hammer: altera prefixo

--- projects/ensino-commons/src/lib/ensino-commons.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { EnsinoCommonsComponent } from './ensino-commons.component';
-
-@NgModule({
-  declarations: [EnsinoCommonsComponent],
-  imports: [],
-  exports: [EnsinoCommonsComponent],
-})
-export class EnsinoCommonsModule {}
