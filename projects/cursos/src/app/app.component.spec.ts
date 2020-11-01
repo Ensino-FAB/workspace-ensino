1cfbeb3b6f46687a90761dd2a83246addfd57ce0
Fernando Tomasio <fernandotomasio@gmail.com>
2020-11-01 08:12:43 -0300

    feat(cursos): :sparkles: implementa layout basico

--- projects/cursos/src/app/app.component.spec.ts
+++ projects/cursos/src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ButtonModule, IconModule } from '@cca-fab/cca-fab-components-common';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, ButtonModule, IconModule],
       declarations: [AppComponent],
     }).compileComponents();
   });
