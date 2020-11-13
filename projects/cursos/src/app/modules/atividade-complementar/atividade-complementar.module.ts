8130ddc175a32e3d3b8b458299e30ad51747324e
colacobruno <bruno@colaco.com.br>
2020-11-13 14:39:51 -0300

    fix(curso-arquivos): altera o esquema de pastas para o padrão

--- projects/cursos/src/app/modules/atividade-complementar/atividade-complementar.module.ts
+++ projects/cursos/src/app/modules/atividade-complementar/atividade-complementar.module.ts
@@ -1,13 +1,6 @@
 import { ATIVIDADE_COMPLEMENTAR_ROUTES } from './atividade-complementar-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AtividadeComplementarSearchComponent } from './components/atividade-complementar-search/atividade-complementar-search.component';
-import { AtividadeComplementarListComponent } from './components/atividade-complementar-list/atividade-complementar-list.component';
-import { AtividadeComplementarFormComponent } from './components/atividade-complementar-form/atividade-complementar-form.component';
-import { AtividadeComplementarDetailComponent } from './components/atividade-complementar-detail/atividade-complementar-detail.component';
-import { AtividadeComplementarEditComponent } from './containers/atividade-complementar-edit/atividade-complementar-edit.component';
-import { AtividadeComplementarIndexComponent } from './containers/atividade-complementar-index/atividade-complementar-index.component';
-import { AtividadeComplementarShowcaseComponent } from './containers/atividade-complementar-showcase/atividade-complementar-showcase.component';
 
 import {
   TooltipModule,
@@ -24,16 +17,19 @@ import {
   InputModule,
 } from '@cca-fab/cca-fab-components-common';
 import { RouterModule } from '@angular/router';
+import { GestaoComponent } from './containers/gestao/gestao.component';
+import { CadastroComponent } from './containers/cadastro/cadastro.component';
+import { ConsultaComponent } from './containers/consulta/consulta.component';
+import { EdicaoComponent } from './containers/edicao/edicao.component';
+import { DetalheComponent } from './containers/detalhe/detalhe.component';
 
 @NgModule({
   declarations: [
-    AtividadeComplementarSearchComponent,
-    AtividadeComplementarListComponent,
-    AtividadeComplementarFormComponent,
-    AtividadeComplementarDetailComponent,
-    AtividadeComplementarEditComponent,
-    AtividadeComplementarIndexComponent,
-    AtividadeComplementarShowcaseComponent,
+    GestaoComponent,
+    CadastroComponent,
+    ConsultaComponent,
+    EdicaoComponent,
+    DetalheComponent,
   ],
   imports: [
     CommonModule,
--- projects/cursos/src/app/modules/curso/containers/edicao/edicao.component.spec.ts
+++ projects/cursos/src/app/modules/curso/containers/edicao/edicao.component.spec.ts
@@ -1,5 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { EdicaoComponent } from './edicao.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { OverlayModule } from '@angular/cdk/overlay';
+import {
+  ButtonModule,
+  FormModule,
+  InputModule,
+  LabelModule,
+  SelectModule,
+  TextareaModule,
+} from '@cca-fab/cca-fab-components-common';
 
 describe('EdicaoComponent', () => {
   let component: EdicaoComponent;
@@ -8,6 +19,17 @@ describe('EdicaoComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [EdicaoComponent],
+      imports: [
+        NoopAnimationsModule,
+        OverlayModule,
+        ButtonModule,
+        FormModule,
+        InputModule,
+        LabelModule,
+        SelectModule,
+        TextareaModule,
+        RouterTestingModule,
+      ],
     }).compileComponents();
   });
 
