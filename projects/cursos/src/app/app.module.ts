1cd7da72da4950f7311bc96a8ed02cb69f7f6a7a
Fernando Tomasio <fernandotomasio@@gmail.com>
2020-11-18 08:11:23 -0300

    feat(cursos): :sparkles: configura autenticaçao keycloak

--- projects/cursos/src/app/app.module.ts
+++ projects/cursos/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NavModule } from './../../../ensino-commons/src/lib/components/nav/nav.module';
 import { SubNavModule } from './../../../ensino-commons/src/lib/components/sub-nav/sub-nav.module';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,7 +11,23 @@ import {
   ButtonModule,
   DropmenuModule,
 } from '@cca-fab/cca-fab-components-common';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
 
+function initializeKeycloak(keycloak: KeycloakService): any {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8084/auth',
+        realm: 'FAB',
+        clientId: 'ensino-client',
+      },
+      initOptions: {
+        onLoad: 'login-required',
+        silentCheckSsoRedirectUri:
+          'http://localhost:4200/assets/silent-check-sso.html',
+      },
+    });
+}
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -24,8 +40,16 @@ import {
     DropmenuModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    KeycloakAngularModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService],
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
