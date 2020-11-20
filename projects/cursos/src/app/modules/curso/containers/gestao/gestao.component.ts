b3c35d7c097aa6c403809e5d68a24f0458583019
Davidson Goes <davidsongoes@gmail.com>
2020-11-20 10:00:52 -0300

    style: :lipstick: adiciona animaçao botoes gestoes

--- projects/cursos/src/app/modules/curso/containers/gestao/gestao.component.ts
+++ projects/cursos/src/app/modules/curso/containers/gestao/gestao.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeIn } from 'projects/cursos/src/app/app.animation';
 
 @Component({
   selector: 'app-gestao',
   templateUrl: './gestao.component.html',
   styleUrls: ['./gestao.component.scss'],
+  animations: [fadeIn()],
 })
 export class GestaoComponent implements OnInit {
   constructor() {}
