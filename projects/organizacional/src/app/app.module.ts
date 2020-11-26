cb50ed2b9df2c5c0c5de70632e3327b21073b967
Fernando Tomasio <fernandotomasio@gmail.com>
2020-11-25 21:20:51 -0300

    feat(organizacional): :sparkles: adiciona layout

--- projects/organizacional/src/app/app.module.ts
+++ projects/organizacional/src/app/app.module.ts
@@ -1,13 +1,62 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  IconModule,
+  ButtonModule,
+  DropmenuModule,
+  ToastModule,
+} from '@cca-fab/cca-fab-components-common';
+import {
+  SubNavModule,
+  NavModule,
+  ToastService,
+} from 'projects/ensino-commons/src/public-api';
+import { environment } from '../environments/environment';
 
+function initializeKeycloak(keycloak: KeycloakService): any {
+  return () =>
+    keycloak.init({
+      config: {
+        url: environment.KEYCLOAK_URL,
+        realm: environment.KEYCLOAK_REALM,
+        clientId: environment.KEYCLOAK_CLIENT_ID,
+      },
+      initOptions: {
+        onLoad: 'login-required',
+        silentCheckSsoRedirectUri: environment.KEYCLOAK_REDIRECT_URI,
+      },
+    });
+}
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    IconModule,
+    ButtonModule,
+    SubNavModule,
+    NavModule,
+    DropmenuModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    KeycloakAngularModule,
+    ToastModule,
+  ],
+  providers: [
+    ToastService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService],
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- projects/organizacional/src/environments/environment.ts
+++ projects/organizacional/src/environments/environment.ts
@@ -4,6 +4,12 @@
 
 export const environment = {
   production: false,
+  CURSO_API: 'http://localhost:8080/esistens-organizacional-api',
+  KEYCLOAK_URL: 'http://localhost:8084/auth',
+  KEYCLOAK_REALM: 'FAB',
+  KEYCLOAK_CLIENT_ID: 'ensino-client',
+  KEYCLOAK_REDIRECT_URI:
+    'http://localhost:4200/esistens-organizacional/assets/silent-check-sso.html',
 };
 
 /*
