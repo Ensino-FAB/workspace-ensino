dcb1364444fe6e15a1aac8ad9ef47d9d14dbac19
andreibarroso <andrei.mesquita@globomail.com>
2020-11-26 15:02:14 -0300

    feat(organizacional): :construction: Cria componente gestao

--- projects/organizacional/src/app/modules/organizacao/organizacao.module.ts
+++ projects/organizacional/src/app/modules/organizacao/organizacao.module.ts
@@ -3,9 +3,19 @@ import { CommonModule } from '@angular/common';
 import { ConsultaComponent } from './containers/consulta/consulta.component';
 import { RouterModule } from '@angular/router';
 import { ORGANIZACAO_ROUTES } from './organizacao-routing.module';
+import { DetalheComponent } from './containers/detalhe/detalhe.component';
+import { EdicaoComponent } from './containers/edicao/edicao.component';
+import { GestaoComponent } from './containers/gestao/gestao.component';
+import { CadastroComponent } from './containers/cadastro/cadastro.component';
 
 @NgModule({
-  declarations: [ConsultaComponent],
+  declarations: [
+    ConsultaComponent,
+    CadastroComponent,
+    DetalheComponent,
+    EdicaoComponent,
+    GestaoComponent,
+  ],
   imports: [CommonModule, RouterModule.forChild(ORGANIZACAO_ROUTES)],
 })
 export class OrganizacaoModule {}
