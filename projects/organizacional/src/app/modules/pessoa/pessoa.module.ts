75685909e0d031455651a50b333afa54912ee237
Davidson Goes <davidsongoes@gmail.com>
2020-11-26 10:16:29 -0300

    feat(pessoa): :construction: estrutura pessoa

--- projects/organizacional/src/app/modules/pessoa/pessoa.module.ts
+++ projects/organizacional/src/app/modules/pessoa/pessoa.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ConsultaComponent } from './containers/consulta/consulta.component';
+import { PESSOA_ROUTES } from './pessoa-routing.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [ConsultaComponent],
+  imports: [CommonModule, RouterModule.forChild(PESSOA_ROUTES)],
 })
 export class PessoaModule {}
