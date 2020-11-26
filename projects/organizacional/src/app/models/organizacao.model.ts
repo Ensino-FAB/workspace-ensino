46003288722391ed477dde2880dcc8ea5a50d7c0
andreibarroso <andrei.mesquita@globomail.com>
2020-11-26 16:02:21 -0300

    feat(organizacional): :sparkles: Implementa models

---
+++ projects/organizacional/src/app/models/organizacao.model.ts
@@ -0,0 +1,5 @@
+import { BaseModel } from 'projects/ensino-commons/src/lib/models/base.model';
+
+export interface OrganizacaoModel extends BaseModel {
+  nome: string;
+}
---
+++ projects/organizacional/src/app/models/pessoa.model.ts
@@ -0,0 +1,5 @@
+import { BaseModel } from 'projects/ensino-commons/src/lib/models/base.model';
+
+export interface PessoaModel extends BaseModel {
+  nome: string;
+}
