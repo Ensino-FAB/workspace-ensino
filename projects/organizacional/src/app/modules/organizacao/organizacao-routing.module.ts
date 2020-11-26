94b9b1b99faac1959bebcd422fa5b5d1345cf879
Davidson Goes <davidsongoes@gmail.com>
2020-11-26 10:12:34 -0300

    feat(organizacao): estrutura organizacao

--- projects/organizacional/src/app/modules/organizacao/organizacao-routing.module.ts
+++ projects/organizacional/src/app/modules/organizacao/organizacao-routing.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes } from '@angular/router';
+import { ConsultaComponent } from './containers/consulta/consulta.component';
+
+export const ORGANIZACAO_ROUTES: Routes = [
+  { path: '', component: ConsultaComponent, data: { breadcrumb: null } },
+];
 
 @NgModule({
   declarations: [],
--- projects/organizacional/src/app/modules/organizacao/organizacao.module.ts
+++ projects/organizacional/src/app/modules/organizacao/organizacao.module.ts
@@ -1,19 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConsultaComponent } from './containers/consulta/consulta.component';
-import { CadastroComponent } from './containers/cadastro/cadastro.component';
-import { DetalheComponent } from './containers/detalhe/detalhe.component';
-import { EdicaoComponent } from './containers/edicao/edicao.component';
-import { DetalheBasicoComponent } from './components/detalhe-basico/detalhe-basico.component';
+import { RouterModule } from '@angular/router';
+import { ORGANIZACAO_ROUTES } from './organizacao-routing.module';
 
 @NgModule({
-  declarations: [
-    ConsultaComponent,
-    CadastroComponent,
-    DetalheComponent,
-    EdicaoComponent,
-    DetalheBasicoComponent,
-  ],
-  imports: [CommonModule],
+  declarations: [ConsultaComponent],
+  imports: [CommonModule, RouterModule.forChild(ORGANIZACAO_ROUTES)],
 })
 export class OrganizacaoModule {}
