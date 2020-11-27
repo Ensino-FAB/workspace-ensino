1c8d9480c71da19690587160ee3fbc200556182c
lucaslbatista <lucaslbatista@gmail.com>
2020-11-27 14:35:31 -0300

    feat(organizacao): :construction: implementaçao parcial do cadastro de organizaçoes

--- projects/organizacional/src/app/modules/organizacao/containers/cadastro/cadastro.component.ts
+++ projects/organizacional/src/app/modules/organizacao/containers/cadastro/cadastro.component.ts
@@ -1,12 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { OrganizacaoFacade } from './../../organizacao-facade';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { ToastService } from 'projects/ensino-commons/src/public-api';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
   styleUrls: ['./cadastro.component.scss'],
 })
-export class CadastroComponent implements OnInit {
-  constructor() {}
+export class CadastroComponent implements OnInit, OnDestroy {
+  private subs$: Subscription[] = [];
+  organizacaoForm: FormGroup;
+  formId: 'organizacao-form';
 
-  ngOnInit(): void {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private toast: ToastService,
+    private router: Router,
+    private facade: OrganizacaoFacade
+  ) {}
+
+  ngOnInit(): void {
+    this.organizacaoForm = this.formBuilder.group({
+      nome: ['', Validators.required],
+      sigla: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      pabx: [''],
+      homepage: [''],
+      extinta: ['N'],
+      tipo: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    // if (this.organizacaoForm.valid) {
+    //   this.subs$.push(
+    //     this.facade.(this.organizacaoForm.value).subscribe(() => {
+    //       this.toast.show({
+    //         message: 'A proposta foi salva com sucesso!',
+    //         type: 'success',
+    //       });
+    //       this.router.navigate(['proposta', 'listar']);
+    //     })
+    //   );
+    // } else {
+    //   this.toast.show({
+    //     message: 'Erro ao tentar salvar a Proposta!',
+    //     type: 'alert',
+    //   });
+    // }
+  }
+
+  resetForm(): void {
+    this.organizacaoForm.reset();
+  }
+
+  ngOnDestroy(): void {
+    this.subs$.forEach((sub) => {
+      sub.unsubscribe();
+    });
+  }
 }
--- projects/organizacional/src/app/modules/organizacao/organizacao-facade.ts
+++ projects/organizacional/src/app/modules/organizacao/organizacao-facade.ts
@@ -1,6 +1,16 @@
-import { Injectable } from '@angular/core';
+import { OrganizacaoService } from './../../services/organizacao.service';
+import { Injectable, Injector } from '@angular/core';
 
 @Injectable()
 export class OrganizacaoFacade {
-  constructor() {}
+  private _organizacaoService: OrganizacaoService;
+
+  constructor(private injector: Injector) {}
+
+  private get organizacaoService(): OrganizacaoService {
+    if (!this._organizacaoService) {
+      this._organizacaoService = this.injector.get(OrganizacaoService);
+    }
+    return this._organizacaoService;
+  }
 }
