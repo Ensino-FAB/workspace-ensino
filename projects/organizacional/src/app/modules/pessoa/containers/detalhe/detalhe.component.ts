200d1c69f3a99904f780fb5ee44f746576ea5182
Davidson Goes <davidsongoes@gmail.com>
2020-11-27 08:43:09 -0300

    feat(pessoa): :sparkles: adiciona estrutura crud pessoa

---
+++ projects/organizacional/src/app/modules/pessoa/containers/detalhe/detalhe.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhe',
+  templateUrl: './detalhe.component.html',
+  styleUrls: ['./detalhe.component.scss'],
+})
+export class DetalheComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- projects/organizacional/src/app/modules/pessoa/pessoa.module.ts
+++ projects/organizacional/src/app/modules/pessoa/pessoa.module.ts
@@ -3,9 +3,17 @@ import { CommonModule } from '@angular/common';
 import { ConsultaComponent } from './containers/consulta/consulta.component';
 import { PESSOA_ROUTES } from './pessoa-routing.module';
 import { RouterModule } from '@angular/router';
+import { CadastroComponent } from './containers/cadastro/cadastro.component';
+import { DetalheComponent } from './containers/detalhe/detalhe.component';
+import { EdicaoComponent } from './containers/edicao/edicao.component';
 
 @NgModule({
-  declarations: [ConsultaComponent],
+  declarations: [
+    ConsultaComponent,
+    CadastroComponent,
+    DetalheComponent,
+    EdicaoComponent,
+  ],
   imports: [CommonModule, RouterModule.forChild(PESSOA_ROUTES)],
 })
 export class PessoaModule {}
