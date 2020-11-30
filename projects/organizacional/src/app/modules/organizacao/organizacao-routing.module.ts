ae54234b258fbc0d6c3cd006c8778ed6139927a3
andreibarroso <andrei.mesquita@globomail.com>
2020-11-30 08:48:16 -0300

    feat(organização): :sparkles: Implementa rotas edição e  detalhar

--- projects/organizacional/src/app/modules/organizacao/organizacao-routing.module.ts
+++ projects/organizacional/src/app/modules/organizacao/organizacao-routing.module.ts
@@ -3,12 +3,26 @@ import { CommonModule } from '@angular/common';
 import { Routes } from '@angular/router';
 import { ConsultaComponent } from './containers/consulta/consulta.component';
 import { CadastroComponent } from './containers/cadastro/cadastro.component';
+import { EdicaoComponent } from './containers/edicao/edicao.component';
+import { DetalheComponent } from './containers/detalhe/detalhe.component';
 
 export const ORGANIZACAO_ROUTES: Routes = [
   { path: '', component: ConsultaComponent, data: { breadcrumb: null } },
   {
     path: 'criar',
     component: CadastroComponent,
+    data: { breadcrumb: 'Cadastrar' },
+  },
+
+  {
+    path: 'detalhar',
+    component: DetalheComponent,
+    data: { breadcrumb: 'Cadastrar' },
+  },
+
+  {
+    path: 'editar',
+    component: EdicaoComponent,
     data: { breadcrumb: 'Cadastrar' },
   },
 ];
