fdef69de9fda04f200f46e2948a7151f5db708cd
andreibarroso <andrei.mesquita@globomail.com>
2020-12-01 09:19:34 -0300

    feat(pessoa): :sparkles: Cria componente detalhe basico pessoa

---
+++ projects/organizacional/src/app/modules/pessoa/components/detalhe-basico-pessoa/detalhe-basico-pessoa.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhe-basico-pessoa',
+  templateUrl: './detalhe-basico-pessoa.component.html',
+  styleUrls: ['./detalhe-basico-pessoa.component.scss'],
+})
+export class DetalheBasicoPessoaComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
