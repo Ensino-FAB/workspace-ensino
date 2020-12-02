632aef11d575ba6cec607d3c1b20b6f09ccf5b70
Davidson Goes <davidsongoes@gmail.com>
2020-12-02 14:36:12 -0300

    refactor(pessoa): :art: refatora disposiçao dos dados detalhes

--- projects/organizacional/src/app/modules/pessoa/components/detalhe-basico-pessoa/detalhe-basico-pessoa.component.ts
+++ projects/organizacional/src/app/modules/pessoa/components/detalhe-basico-pessoa/detalhe-basico-pessoa.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { fadeIn } from '../../../../../app/app.animation';
 import { Pessoa } from '../../../../models/pessoa.model';
 
 @Component({
   selector: 'app-detalhe-basico-pessoa',
   templateUrl: './detalhe-basico-pessoa.component.html',
   styleUrls: ['./detalhe-basico-pessoa.component.scss'],
+  animations: [fadeIn()],
 })
 export class DetalheBasicoPessoaComponent implements OnInit {
   @Input() header: string;
