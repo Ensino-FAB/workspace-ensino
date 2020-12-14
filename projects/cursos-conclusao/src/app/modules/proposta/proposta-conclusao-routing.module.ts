ae686eb7ebf58ce8a5efdba85838cb2198436184
AndreiBarroso <andrei.mesquita@globomail.com>
2020-12-14 09:50:40 -0300

    feat(proposta): :sparkles: Configura rotas e module

--- projects/cursos-conclusao/src/app/modules/proposta/proposta-conclusao-routing.module.ts
+++ projects/cursos-conclusao/src/app/modules/proposta/proposta-conclusao-routing.module.ts
@@ -1,8 +1,74 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes } from '@angular/router';
+import { GestaoComponent } from './containers/gestao/gestao.component';
+import { CadastroComponent } from './containers/cadastro/cadastro.component';
+import { FluxoProcessoComponent } from './containers/fluxo-processo/fluxo-processo.component';
+import { EdicaoComponent } from './containers/edicao/edicao.component';
+import { DetalheComponent } from './containers/detalhe/detalhe.component';
+import { DetalheProcessoComponent } from './containers/detalhe-processo/detalhe-processo.component';
+import { ConsultaComponent } from './containers/consulta/consulta.component';
+
+export const PROPOSTA_CONCLUSAO_ROUTES: Routes = [
+  {
+    path: '',
+    component: GestaoComponent,
+    data: { breadcrumb: null },
+  },
+  {
+    path: 'criar',
+    data: {
+      breadcrumb: 'criar',
+    },
+    component: CadastroComponent,
+  },
+  {
+    path: 'etapa/:taskId',
+    component: FluxoProcessoComponent,
+    data: {
+      breadcrumb: 'aprovação de etapa',
+    },
+  },
+  {
+    path: 'editar/:id',
+    data: {
+      breadcrumb: 'editar',
+    },
+    component: EdicaoComponent,
+  },
+  {
+    path: 'detalhes/:id',
+    data: {
+      breadcrumb: 'detalhes',
+    },
+    children: [
+      {
+        path: '',
+        component: DetalheComponent,
+        data: {
+          breadcrumb: null,
+        },
+      },
+      {
+        path: 'processo/:rev',
+        component: DetalheProcessoComponent,
+        data: {
+          breadcrumb: 'processo',
+        },
+      },
+    ],
+  },
+  {
+    path: 'listar',
+    data: {
+      breadcrumb: 'listar',
+    },
+    component: ConsultaComponent,
+  },
+];
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
 })
-export class PropostaConclusaoRoutingModule {}
+export class PropostaConclusaoRoutingModuleRoutingModule {}
--- projects/cursos-conclusao/src/app/modules/proposta/proposta.module.ts
+++ projects/cursos-conclusao/src/app/modules/proposta/proposta.module.ts
@@ -1,25 +1,62 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import {
+  TooltipModule,
+  CardModule,
+  ButtonModule,
+  IconModule,
+  TableModule,
+  ModalModule,
+  TagModule,
+  LabelModule,
+  FormModule,
+  SelectModule,
+  TextareaModule,
+  InputModule,
+} from '@cca-fab/cca-fab-components-common';
+import { PropostaFacade } from './proposta.facade';
 import { CadastroComponent } from './containers/cadastro/cadastro.component';
 import { ConsultaComponent } from './containers/consulta/consulta.component';
+import { DetalheComponent } from './containers/detalhe/detalhe.component';
 import { EdicaoComponent } from './containers/edicao/edicao.component';
-import { FluxoProcessoComponent } from './containers/fluxo-processo/fluxo-processo.component';
 import { GestaoComponent } from './containers/gestao/gestao.component';
 import { DetalheProcessoComponent } from './containers/detalhe-processo/detalhe-processo.component';
-import { DetalheComponent } from './containers/detalhe/detalhe.component';
+import { BpmnViewerModule } from 'projects/ensino-commons/src/public-api';
+import { FluxoProcessoComponent } from './containers/fluxo-processo/fluxo-processo.component';
+import { PROPOSTA_CONCLUSAO_ROUTES } from './proposta-conclusao-routing.module';
+import { PROPOSTA_ROUTES } from '../../../../../cursos/src/app/modules/proposta/proposta-routing.module';
 import { DetalheBasicoConclusaoComponent } from './components/detalhe-basico-conclusao/detalhe-basico-conclusao.component';
 
 @NgModule({
   declarations: [
     CadastroComponent,
     ConsultaComponent,
-    EdicaoComponent,
-    FluxoProcessoComponent,
-    GestaoComponent,
-    DetalheProcessoComponent,
     DetalheComponent,
+    EdicaoComponent,
+    GestaoComponent,
     DetalheBasicoConclusaoComponent,
+    DetalheProcessoComponent,
+    FluxoProcessoComponent,
   ],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(PROPOSTA_CONCLUSAO_ROUTES),
+    ButtonModule,
+    IconModule,
+    TooltipModule,
+    CardModule,
+    TableModule,
+    ModalModule,
+    TagModule,
+    LabelModule,
+    FormModule,
+    SelectModule,
+    TextareaModule,
+    InputModule,
+    BpmnViewerModule,
+  ],
+  providers: [PropostaFacade],
 })
-export class PropostaModule {}
+export class PropostaConclusaoModule {}
