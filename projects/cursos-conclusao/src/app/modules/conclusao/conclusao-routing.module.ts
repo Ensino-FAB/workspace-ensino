abe915da5bd61ee39902157e1fb60e9bc1d76ba5
andreibarroso <andrei.mesquita@globomail.com>
2020-12-16 15:35:10 -0300

    feat(conclusao): :sparkles: Implementa detalhe basico conclusao

--- projects/cursos-conclusao/src/app/modules/conclusao/conclusao-routing.module.ts
+++ projects/cursos-conclusao/src/app/modules/conclusao/conclusao-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes } from '@angular/router';
 
 import { CadastroComponent } from './containers/cadastro/cadastro.component';
 import { ConsultaComponent } from './containers/consulta/consulta.component';
+import { DetalheComponent } from './containers/detalhe/detalhe.component';
 
 export const CONCLUSAO_ROUTES: Routes = [
   {
@@ -17,6 +18,14 @@ export const CONCLUSAO_ROUTES: Routes = [
       breadcrumb: 'cadastro',
     },
     component: CadastroComponent,
+  },
+
+  {
+    path: 'detalhar/:id',
+    data: {
+      breadcrumb: 'detalhar',
+    },
+    component: DetalheComponent,
   },
 ];
 
--- projects/cursos-conclusao/src/app/modules/conclusao/containers/detalhe/detalhe.component.ts
+++ projects/cursos-conclusao/src/app/modules/conclusao/containers/detalhe/detalhe.component.ts
@@ -1,12 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription, timer } from 'rxjs';
+import { ConclusaoFacade } from '../../conclusao.facade';
+import { ActivatedRoute } from '@angular/router';
+import { mapTo, mergeAll, share, takeUntil } from 'rxjs/operators';
+import { of } from 'rxjs/internal/observable/of';
+import { fadeIn } from '../../../../../../../ensino-commons/src/lib/utils/animation';
+import { ConclusaoCursoResponse } from '../../../../models/conclusao-curso-response.model';
 
 @Component({
   selector: 'app-detalhe',
   templateUrl: './detalhe.component.html',
   styleUrls: ['./detalhe.component.scss'],
+  animations: [fadeIn()],
 })
 export class DetalheComponent implements OnInit {
-  constructor() {}
+  private subs$: Subscription[] = [];
+  public isLoading = false;
+  public id: number;
+  public conclusao: ConclusaoCursoResponse;
 
-  ngOnInit(): void {}
+  constructor(private route: ActivatedRoute, private facade: ConclusaoFacade) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.id = params.id;
+
+      const getOrganizacao$ = this.facade.conclusaoService
+        .find(this.id)
+        .pipe(share());
+      const isLoading$ = of(
+        timer(150).pipe(mapTo(true), takeUntil(getOrganizacao$)),
+        getOrganizacao$.pipe(mapTo(false))
+      ).pipe(mergeAll());
+
+      this.subs$.push(
+        isLoading$.subscribe((status) => {
+          this.isLoading = status;
+        }),
+        getOrganizacao$.subscribe((item) => {
+          if (item) {
+            this.conclusao = item;
+          }
+        })
+      );
+    });
+  }
 }
