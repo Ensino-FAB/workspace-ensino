2f998dff4186c2496857ebd10b3b35031aabdec7
andreibarroso <andrei.mesquita@globomail.com>
2020-12-16 10:24:53 -0300

    feat(conclusao): :sparkles: Implementa consulta conclusao

--- projects/cursos-conclusao/src/app/modules/conclusao/conclusao.facade.ts
+++ projects/cursos-conclusao/src/app/modules/conclusao/conclusao.facade.ts
@@ -1,6 +1,10 @@
 import { Injectable, Injector } from '@angular/core';
 import { ConclusaoService } from '../../services/conclusao.service';
 import { PessoaService } from '../../services/pessoa.service';
+import { PropostaConclusaoSearch } from '../../models/proposta-conclusao-search.model';
+import { Observable } from 'rxjs';
+import { Pageable } from '../../../../../ensino-commons/src/lib/models/pageable.model';
+import { ConclusaoCursoResponse } from '../../models/conclusao-curso-response.model';
 
 @Injectable()
 export class ConclusaoFacade {
@@ -15,10 +19,25 @@ export class ConclusaoFacade {
     }
     return this._ConclusaoService;
   }
+
   public get pessoaService(): PessoaService {
     if (!this._PessoaService) {
       this._PessoaService = this.injector.get(PessoaService);
     }
     return this._PessoaService;
+  }
+
+  public getAlConclusao(
+    search: PropostaConclusaoSearch
+  ): Observable<Pageable<ConclusaoCursoResponse>> {
+    return this.conclusaoService.findAll(search);
+  }
+
+  public findByConclusao(id: number): Observable<ConclusaoCursoResponse> {
+    return this.conclusaoService.findById(id);
+  }
+
+  public delete(id: number): Observable<any> {
+    return this.conclusaoService.remove(id);
   }
 }
--- projects/cursos-conclusao/src/app/modules/conclusao/containers/consulta/consulta.component.ts
+++ projects/cursos-conclusao/src/app/modules/conclusao/containers/consulta/consulta.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { fadeIn } from 'projects/ensino-commons/src/public-api';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { fadeIn, ToastService } from 'projects/ensino-commons/src/public-api';
+import { of, Subscription, timer } from 'rxjs';
+import { FormControl, FormGroup } from '@angular/forms';
+import { TableColumn } from '@cca-fab/cca-fab-components-common';
+import { mapTo, mergeAll, share, takeUntil } from 'rxjs/operators';
+import { ConclusaoFacade } from '../../conclusao.facade';
 
 @Component({
   selector: 'app-consulta',
@@ -7,8 +12,195 @@ import { fadeIn } from 'projects/ensino-commons/src/public-api';
   styleUrls: ['./consulta.component.scss'],
   animations: [fadeIn()],
 })
-export class ConsultaComponent implements OnInit {
-  constructor() {}
+export class ConsultaComponent implements OnInit, OnDestroy {
+  private subs$: Subscription[] = [];
 
-  ngOnInit(): void {}
+  public isLoading = false;
+
+  conclusaoSearch = new FormGroup({
+    capacitacao: new FormControl(''),
+    pessoa: new FormControl(''),
+    local: new FormControl(''),
+  });
+
+  columns: TableColumn[] = [
+    {
+      field: 'capacitacao',
+      title: 'Capacitação',
+      width: '20%',
+    },
+
+    {
+      field: 'local',
+      title: 'Local',
+      width: '20%',
+    },
+
+    {
+      field: 'dtInicio',
+      title: 'Data de Início',
+      width: '12%',
+    },
+    {
+      field: 'dtFim',
+      title: 'Data de fim',
+      width: '12%',
+    },
+
+    {
+      field: 'pessoa',
+      title: 'Pessoa',
+      width: '20%',
+    },
+  ];
+
+  data = [];
+  loadindMockData = new Array(10).fill({
+    capacitacao: '',
+    local: '',
+    dtFim: '',
+    dtInicio: '',
+    status: '',
+  });
+
+  options: object[];
+  optionsStatus: object[];
+
+  asc = true;
+  pageSize = 20;
+  page = 1;
+  count: number;
+  orderBy: string[] = ['id'];
+  totalPages = 1;
+
+  constructor(
+    private facade: ConclusaoFacade,
+    private toastService: ToastService
+  ) {}
+
+  ngOnInit(): void {
+    this.options = [{ name: 'Capacitação', value: 'capacitacao' }];
+    this.options = [{ name: 'Pessoa', value: 'pessoa' }];
+    this.refresh();
+  }
+
+  // tslint:disable-next-line: typedef
+  refresh() {
+    const search = {
+      ...this.conclusaoSearch.value,
+      page: this.page ? this.page - 1 : 0,
+      size: this.pageSize,
+      sort: this.orderBy.map((item) => (this.asc ? item : item + ',desc')),
+    };
+
+    const getConclusao$ = this.facade.getAlConclusao(search).pipe(share());
+    const isLoading$ = of(
+      timer(150).pipe(mapTo(true), takeUntil(getConclusao$)),
+      getConclusao$.pipe(mapTo(false))
+    ).pipe(mergeAll());
+
+    this.subs$.push(
+      isLoading$.subscribe((status) => {
+        this.isLoading = status;
+      }),
+      getConclusao$.subscribe((res) => {
+        this.count = res.totalElements;
+
+        this.data = res.content.map((item) => ({
+          id: `${item?.id}`,
+          capacitacao: `${item?.capacitacaoResponse.nome}`,
+          local: `${item?.local}`,
+          dtInicio: `${item?.dtInicio}`,
+          dtFim: `${item?.dtfim}`,
+          pessoa: `${item?.pessoaResponse.nome}`,
+        }));
+
+        this.totalPages = Math.ceil(this.count / this.pageSize);
+      })
+    );
+  }
+
+  // tslint:disable-next-line: typedef
+  handlePageSizeChange(newSize: number) {
+    this.pageSize = newSize;
+    this.page = 1;
+    this.refresh();
+  }
+
+  // tslint:disable-next-line: typedef
+  onFirstPage() {
+    this.page = 1;
+    this.refresh();
+  }
+
+  // tslint:disable-next-line: typedef
+  onLastPage() {
+    this.page = this.totalPages;
+    this.refresh();
+  }
+
+  // tslint:disable-next-line: typedef
+  handlePageIndexChange(page: number) {
+    this.page = page;
+    this.refresh();
+  }
+
+  // tslint:disable-next-line: typedef
+  handleNextPage() {
+    this.page = Math.min(this.page + 1, this.totalPages);
+    this.refresh();
+  }
+
+  // tslint:disable-next-line: typedef
+  handlePreviousPage() {
+    this.page = Math.max(this.page - 1, 1);
+    this.refresh();
+  }
+
+  // tslint:disable-next-line: typedef
+  handleSortChange(a) {
+    this.page = 1;
+    if (a === null) {
+      this.orderBy = ['id'];
+    } else {
+      this.orderBy = [a];
+    }
+    this.refresh();
+  }
+
+  // tslint:disable-next-line: typedef
+  handleInvertSort() {
+    this.asc = !this.asc;
+    this.refresh();
+  }
+
+  // tslint:disable-next-line: typedef
+  onSubmit() {
+    this.page = 1;
+    this.refresh();
+  }
+
+  // tslint:disable-next-line: typedef
+  clean() {
+    this.conclusaoSearch.reset();
+    this.refresh();
+  }
+
+  onDelete(id: number): void {
+    this.subs$.push(
+      this.facade.delete(id).subscribe(() => {
+        this.refresh();
+        this.toastService.show({
+          message: 'Conclusão deletada com sucesso!',
+          type: 'success',
+        });
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subs$.forEach((sub) => {
+      sub.unsubscribe();
+    });
+  }
 }
