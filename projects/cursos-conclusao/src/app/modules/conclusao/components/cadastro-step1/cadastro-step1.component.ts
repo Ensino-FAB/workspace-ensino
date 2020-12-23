3440c823aaf131709cdb1a67f9a6956c396d37e4
Davidson Góes <davidsongoes@gmail.com>
2020-12-23 11:16:38 -0300

    refactor(conclusao): :hammer: refatora form autocomplete capacitacao step1

--- projects/cursos-conclusao/src/app/modules/conclusao/components/cadastro-step1/cadastro-step1.component.ts
+++ projects/cursos-conclusao/src/app/modules/conclusao/components/cadastro-step1/cadastro-step1.component.ts
@@ -18,9 +18,8 @@ import { ConclusaoFacade } from '../../conclusao.facade';
 })
 export class CadastroStep1Component implements OnInit, OnDestroy {
   private subs$: Subscription[] = [];
-
   capacitacaoOptions: SelectOption[] = [];
-
+  options: SelectOption[] = [];
   @Output() next = new EventEmitter();
   @Input() form: FormGroup;
 
@@ -33,26 +32,38 @@ export class CadastroStep1Component implements OnInit, OnDestroy {
   reload(search = {}): void {
     this.capacitacaoOptions = [];
     this.subs$.push(
-      this.facade.capacitacaoService.findAll(search).subscribe((response) =>
+      this.facade.capacitacaoService.findAll(search).subscribe((response) => {
         response.content.map((capacitacao) => {
           this.capacitacaoOptions.push({
             name: capacitacao.codigo + ' - ' + capacitacao.nome,
             value: capacitacao.id,
           });
-        })
-      )
+        });
+        this.options = [...this.capacitacaoOptions];
+      })
     );
   }
 
-  confirmed(event): void {}
-
-  filter(event): void {
-    const cursoName: string = event;
-
-    if (cursoName.length > 3) {
-      this.reload({ nome: cursoName });
-    }
+  filter(value): any {
+    return this.capacitacaoOptions.filter((option: any | null) =>
+      typeof option === 'string'
+        ? option.toLowerCase().startsWith(value.toLowerCase())
+        : option.name.toLowerCase().startsWith(value.toLowerCase())
+    );
   }
+
+  update(value: any[]) {
+    this.options.splice(0, this.options.length, ...value);
+    return this.options;
+  }
+
+  // filter(event): void {
+  //   const cursoName: string = event;
+
+  //   if (cursoName.length > 3) {
+  //     this.reload({ nome: cursoName });
+  //   }
+  // }
 
   onSubmit(): void {}
 
